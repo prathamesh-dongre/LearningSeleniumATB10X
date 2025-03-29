0847bca4e3d119c6a674d711e7977b1b2d1b7278
Prathamesh <prathameshdongre0@gmail.com>
2025-03-30 00:37:55 +0530

    Initial commit

---
+++ src/test/java/Prathamesh/QA/com/EX01_Selenium_Basics/TestSelenium_01.java
@@ -0,0 +1,18 @@
+package Prathamesh.QA.com.EX01_Selenium_Basics;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium_01 {
+
+    @Test
+    public void test_VerifyVWOLogin() {
+        // Write the code which will execute the perform the ui interactions
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://app.vwo.com");
+        System.out.println(driver.getSessionId());
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/Prathamesh/QA/com/EX01_Selenium_Basics/TestSelenium_02.java
@@ -0,0 +1,24 @@
+package Prathamesh.QA.com.EX01_Selenium_Basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
+
+public class TestSelenium_02 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ChromeDriver Chromium = new ChromeDriver();
+        Chromium.get("https://www.facebook.com/");
+        Chromium.manage().window().maximize();
+
+        WebElement Forgetpass = Chromium.findElement(By.xpath("//a[contains(@href, 'www.facebook.com/recover/initiate/')]"));
+        Forgetpass.click();
+        Thread.sleep(5000);
+        Chromium.quit();
+
+
+    }
+}
